8ba32b635f1f3fbd2edcb2890382056e9d8cc524
Fedor Odintsov <fadmin@list.ru>
2021-05-09 22:36:12 +0300

    feat: implement streams and transform

--- caesar-cipher/caesar-cipher-cli.js
+++ caesar-cipher/caesar-cipher-cli.js
@@ -1,4 +1,36 @@
+import { pipeline } from 'stream';
+import fs from 'fs';
+
 import processArgs from './processArgs.js';
+import Transformer from "./transform.js";
 
 const args = processArgs();
-console.log(args);
+const encodeSign = args['action'] === 'encode' ? 1 : -1;
+
+let inputStream;
+let outputStream;
+let transformStream = new Transformer({ shift: args['shift'] * encodeSign });
+
+if (args['input'] === undefined || args['input'] === true) {
+  inputStream = process.stdin;
+} else {
+  inputStream = fs.createReadStream(args['input']);
+}
+
+if (args['output'] === undefined || args['output'] === true) {
+  outputStream = process.stdout;
+} else {
+  outputStream = fs.createWriteStream(args['output']);
+}
+
+pipeline(
+  inputStream,
+  transformStream,
+  outputStream,
+  (err) => {
+    if (err) {
+      console.error('Pipeline failed.', err);
+      process.exit(1);
+    }
+  }
+);

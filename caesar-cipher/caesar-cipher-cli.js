ca037ecff0ae1ff81b7d01cae05f03af6161f076
Fedor Odintsov <fadmin@list.ru>
2021-05-09 23:27:47 +0300

    feat: check existanse of files

--- caesar-cipher/caesar-cipher-cli.js
+++ caesar-cipher/caesar-cipher-cli.js
@@ -11,17 +11,49 @@ let inputStream;
 let outputStream;
 let transformStream = new Transformer({ shift: args['shift'] * encodeSign });
 
+function checkFileExists(file, flags) {
+  let fileExists = true;
+  try {
+    fs.accessSync(file, flags);
+  } catch (err) {
+    fileExists = false;
+  }
+  return fileExists;
+}
+
 if (args['input'] === undefined || args['input'] === true) {
   inputStream = process.stdin;
 } else {
-  inputStream = fs.createReadStream(args['input']);
+  if (checkFileExists(args['input'], fs.constants.F_OK | fs.constants.R_OK)) {
+    inputStream = fs.createReadStream(args['input']);
+  } else {
+    console.error(`Error! File ${args['input']} is not exist or inaccessible for reading.`);
+    process.exit(1);
+  }
 }
+
+inputStream.on('error', (err) => {
+  console.error(`Error! Something has happened with input stream`);
+  process.exit(1);
+});
 
 if (args['output'] === undefined || args['output'] === true) {
   outputStream = process.stdout;
 } else {
-  outputStream = fs.createWriteStream(args['output'], {flags: 'a'});
+  if (checkFileExists(args['output'], fs.constants.F_OK | fs.constants.W_OK)) {
+    outputStream = fs.createWriteStream(args['output'], {flags: 'a'});
+  } else {
+    console.error(`Error! File ${args['output']} is not exist or inaccessible for writing.`);
+    process.exit(1);
+  }
 }
+
+outputStream.on('error', (err) => {
+  console.error(`Error! Something has happened with output stream`);
+  process.exit(1);
+});
+
+
 
 pipeline(
   inputStream,
